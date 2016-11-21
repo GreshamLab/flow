e2d40d4116ab91161e49297da157613339348240
darachm <dchmiller@gmail.com>
2016-11-21 16:04:27 -0500

    still fiddling...

---
+++ src/requireInstall.R
@@ -0,0 +1,13 @@
+requireInstall <- function(packageName,isBioconductor=F) {
+  if ( !try(require(packageName,character.only=T)) ) {
+    print(paste0("You don't have ",packageName," accessible, ",
+      "I'm gonna install it"))
+    if (isBioconductor) {
+      source("http://bioconductor.org/biocLite.R")                        
+      biocLite(packageName)                                                 
+    } else {
+      install.packages("packageName", repos = "http://cran.us.r-project.org")
+    }
+  }
+  return(1)
+}
